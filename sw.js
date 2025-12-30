2278b3b2dab8c677f8cd8acdf6387953f7736e0e
dbs <58137288+dhillon-balminder-singh@users.noreply.github.com>
2025-12-30 16:17:22 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'geo-map-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// Install: Cache the app Shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
